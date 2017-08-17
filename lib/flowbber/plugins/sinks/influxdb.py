0d2958cf9c4d948422268564fbf919299823a74b
Carlos Jenkins <carlos.jenkins@kuralabs.io>
2017-08-17 17:01:55 -0600

    new: dev: Added a data flattening algorithm to preprocess data to InfluxDB.

--- lib/flowbber/plugins/sinks/influxdb.py
+++ lib/flowbber/plugins/sinks/influxdb.py
@@ -22,13 +22,64 @@ Simple InfluxDB storage sink plugin.
 from logging import getLogger
 from datetime import datetime
 
+from pprintpp import pformat
+
 from flowbber.entities import Sink
 
 
 log = getLogger(__name__)
 
 
-def transform_to_points(timestamp, data):
+def transform_to_flat(data, keysjoiner, keysjoinerreplace):
+    """
+    """
+    influxdb_supported = (str, int, float, bool)
+
+    def joinkey(path, key):
+        parts = [*path, key]
+
+        if keysjoinerreplace is not None:
+            parts = [
+                part.replace(keysjoiner, keysjoinerreplace)
+                for part in parts
+            ]
+
+        return keysjoiner.join(parts)
+
+    def flat(dictionary, path):
+        assert isinstance(dictionary, dict)
+
+        for key, value in dictionary.items():
+            if isinstance(value, influxdb_supported):
+                yield joinkey(path, key), value
+                continue
+
+            if isinstance(value, dict):
+                yield from flat(value, path + [key])
+                continue
+
+            if isinstance(value, list):
+                value_as_dict = {
+                    str(index): subvalue
+                    for index, subvalue in enumerate(value)
+                }
+                yield from flat(value_as_dict, path)
+                continue
+
+            raise ValueError('Unable to flat value ({}): {}'.format(
+                type(value), value
+            ))
+
+    return {
+        key: {
+            subkey: subvalue
+            for subkey, subvalue in flat(value, [])
+        }
+        for key, value in data.items()
+    }
+
+
+def transform_to_points(timestamp, flat):
     """
     FIXME: Document.
     FIXME: Transform deeper collected values into flat structure.
@@ -46,6 +97,8 @@ def transform_to_points(timestamp, data):
                 "value": 0.64
             }
         }
+
+    Field value must be string, float, integer, or boolean.
     """
     points = [
         {
@@ -53,11 +106,11 @@ def transform_to_points(timestamp, data):
             'tags': {},
             'time': timestamp,
             'fields': {
-                value: key
-                for value, key in collected.items()
+                key: value
+                for key, value in collected.items()
             }
         }
-        for source, collected in data.items()
+        for source, collected in flat.items()
     ]
 
     return points
@@ -65,9 +118,6 @@ def transform_to_points(timestamp, data):
 
 class InfluxDBSink(Sink):
     def declare_config(self, config):
-        """
-        FIXME: Document.
-        """
 
         # Connection options
         config.add_option(
@@ -134,6 +184,20 @@ class InfluxDBSink(Sink):
             type=lambda opt: None if opt is None else str(opt),
         )
 
+        config.add_option(
+            'keysjoiner',
+            default='.',
+            optional=True,
+            type=str,
+        )
+
+        config.add_option(
+            'keysjoinerreplace',
+            default=':',
+            optional=True,
+            type=lambda opt: None if opt is None else str(opt),
+        )
+
         # Check if uri is defined and if so then delete other keys
         def custom_validator(validated):
             if validated['uri'] is not None:
@@ -179,9 +243,21 @@ class InfluxDBSink(Sink):
                 verify_ssl=self.config.verify_ssl.value,
             )
 
+        # Flat data
+        log.info('Flattening data for InfluxDB')
+        flat = transform_to_flat(
+            data,
+            self.config.keysjoiner.value,
+            self.config.keysjoinerreplace.value,
+        )
+        del data  # Release a bit of ram here
+        log.debug('Flat data:\n{}'.format(pformat(flat)))
+
         # Insert points
         log.info('Creating points using timestamp {}'.format(key))
-        points = transform_to_points(key, data)
+        points = transform_to_points(key, flat)
+        del flat  # Release a bit of ram here
+        log.debug('Points data:\n{}'.format(pformat(points)))
 
         log.info('Inserting points to InfluxDB: {}'.format([
             point['measurement'] for point in points
