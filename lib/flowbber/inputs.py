5a1542b65d8405178acbd974ef99b5c3857e9529
Carlos Jenkins <carlos.jenkins@kuralabs.io>
2017-08-23 18:44:29 -0600

    new: dev: Added validation of the pipeline definition.

--- lib/flowbber/inputs.py
+++ lib/flowbber/inputs.py
@@ -19,6 +19,62 @@
 Input pipeline definition formats parses.
 """
 
+from pprintpp import pformat
+from cerberus import Validator
+
+from .logging import get_logger
+
+
+SLUG_SCHEMA = {
+    'required': True,
+    'type': 'string',
+    'regex': r'^[a-zA-Z0-9_-]+$',
+}
+
+
+ENTITY_SCHEMA = {
+    'type': SLUG_SCHEMA,
+    'id': SLUG_SCHEMA,
+    'config': {
+        'required': False,
+        'type': 'dict',
+    },
+}
+
+
+PIPELINE_SCHEMA = {
+    'sources': {
+        'required': True,
+        'type': 'list',
+        'empty': False,
+        'schema': {
+            'type': 'dict',
+            'schema': ENTITY_SCHEMA,
+        },
+    },
+    'aggregators': {
+        'required': False,
+        'type': 'list',
+        'empty': True,
+        'schema': {
+            'type': 'dict',
+            'schema': ENTITY_SCHEMA,
+        },
+    },
+    'sinks': {
+        'required': True,
+        'type': 'list',
+        'empty': False,
+        'schema': {
+            'type': 'dict',
+            'schema': ENTITY_SCHEMA,
+        },
+    },
+}
+
+
+log = get_logger(__name__)
+
 
 def load_pipeline_json(path):
     """
@@ -53,16 +109,28 @@ def load_pipeline(path):
             )
         )
 
-    definition = supported_formats[extension](path)
+    try:
+        definition = supported_formats[extension](path)
+    except Exception as e:
+        log.critical('Unable to parse pipeline definition {}'.format(path))
+        raise e
+
+    # Validate data structure
+    validator = Validator(PIPELINE_SCHEMA)
+    if not validator.validate(definition):
+        log.critical(
+            'Invalid pipeline definition:\n{}'.format(
+                pformat(validator.errors)
+            )
+        )
+        raise SyntaxError('Invalid pipeline definition')
 
     # Add an empty aggregators list
     # Aggregators are the only entities that are allowed empty
     if 'aggregators' not in definition:
         definition['aggregators'] = []
 
-    # Validate data structure
-    # FIXME: Implement
-
+    log.info('Pipeline definition loaded and validated')
     return definition
 
 
