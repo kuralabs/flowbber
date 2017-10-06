033229f157d1de9b327e0814af0b3f890ec9866a
Carlos Jenkins <carlos.jenkins@kuralabs.io>
2017-10-06 01:32:18 -0600

    new: dev: Added a test that runs all the examples.

---
+++ test/test_examples.py
@@ -0,0 +1,49 @@
+from os import environ
+from pathlib import Path
+from subprocess import run
+from collections import namedtuple
+
+from pytest import mark, skip
+
+from flowbber.main import main
+from flowbber.logging import get_logger, setup_logging
+
+
+Arguments = namedtuple('Arguments', 'pipeline')
+
+
+log = get_logger(__name__)
+examples = Path(__file__).parent.parent / 'examples'
+
+
+def setup_module(module):
+    setup_logging(verbosity=2)
+
+
+@mark.parametrize(['name', 'pipelinedef'], [
+    ['github', 'pipeline.toml'],
+    ['sloc', 'pipeline.toml'],
+    ['basic', 'pipeline.toml'],
+    ['local', 'pipeline.toml'],
+    ['advanced', 'pipeline.json'],
+    ['advanced', 'pipeline.toml'],
+    ['test', 'pipeline.toml'],
+])
+def test_pipelines(name, pipelinedef):
+    # Exceptions ...
+    if name == 'github':
+        if 'GITHUBTOKEN' not in environ:
+            skip('Missing GITHUBTOKEN environment variable')
+
+    # Run pipeline
+    args = Arguments(examples / name / pipelinedef)
+    result = main(args)
+    assert result == 0
+
+
+@mark.parametrize(['script'], [
+    ['cpud/cpud.py'],
+    ['speedd/speedd.py'],
+])
+def test_daemons(script):
+    run(str(examples / script), check=True)
